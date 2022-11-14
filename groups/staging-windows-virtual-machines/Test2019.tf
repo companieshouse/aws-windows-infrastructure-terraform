6cd183b535a80fd0aadda7852c77b358dd0783b3
Adrian Davey <adavey@companieshouse.gov.uk>
2022-11-14 11:17:54 +0000

    Security Variables updated for test2019 vm

--- groups/staging-windows-virtual-machines/Test2019.tf
+++ groups/staging-windows-virtual-machines/Test2019.tf
@@ -1,6 +1,94 @@
 # ------------------------------------------------------------------------------
 # Test 2019 server 1 Security Group and rules
 # ------------------------------------------------------------------------------
+module "test_2019_1_ec2_security_group" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "~> 3.0"
+
+  name        = "sgr-${var.application}-test-2019-1-server"
+  description = "Security group for the ${var.application} Test 2019 Server 1"
+  vpc_id      = data.aws_vpc.vpc.id
+
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 135
+      to_port     = 135
+      protocol    = "tcp"
+      cidr_blocks = join(",", local.test_2019_1_135_cidr_block)
+    },
+    {
+      from_port   = 445
+      to_port     = 445
+      protocol    = "tcp"
+      cidr_blocks = "172.16.101.82/32"
+    },
+    {
+      from_port   = 1000
+      to_port     = 1000
+      protocol    = "tcp"
+      cidr_blocks = "172.16.200.58/32"
+    },
+    {
+      rule        = "rdp-tcp"
+      cidr_blocks = join(",", local.test_2019_1_rdp_cidr_block)
+    },
+    {
+      rule        = "winrm-http-tcp"
+      cidr_blocks = "172.16.200.200/32"
+    },
+    {
+      from_port   = 49155
+      to_port     = 49155
+      protocol    = "tcp"
+      cidr_blocks = join(",", local.test_2019_1_49155_cidr_block)
+    },
+    {
+      from_port   = 49377
+      to_port     = 49377
+      protocol    = "tcp"
+      cidr_blocks = "172.16.101.82/32"
+    },
+    {
+      from_port   = 58543
+      to_port     = 58543
+      protocol    = "tcp"
+      cidr_blocks = "172.16.200.26/32"
+    },
+    {
+      from_port   = 58577
+      to_port     = 58577
+      protocol    = "tcp"
+      cidr_blocks = "172.16.101.82/32"
+    },
+    {
+      rule        = "http-8080-tcp"
+      cidr_blocks = join(",", local.test_2019_1_8080_cidr_block)
+    },
+    {
+      from_port   = 6400
+      to_port     = 6414
+      protocol    = "tcp"
+      cidr_blocks = join(",", local.test_2019_1_6400_6414_cidr_block)
+    },
+    {
+      from_port   = 135
+      to_port     = 135
+      protocol    = "tcp"
+      description = "WMI Access"
+      cidr_blocks = join(",", local.azure_dc_cidrs)
+    },
+    {
+      from_port   = 49152
+      to_port     = 65535
+      protocol    = "tcp"
+      description = "WMI Access"
+      cidr_blocks = join(",", local.azure_dc_cidrs)
+    }
+  ]
+
+  egress_rules = ["all-all"]
+}
+
 
 module "test_2019_1_ec2" {
   source  = "terraform-aws-modules/ec2-instance/aws"
