0e86a4d1b1883e28eabf0dc81fffd6bdd701fd61
Adrian Davey <adavey@companieshouse.gov.uk>
2022-11-28 13:15:20 +0000

    test2019 - testing - 24 vm - deploy

--- groups/staging-windows-virtual-machines/Test2019.tf
+++ groups/staging-windows-virtual-machines/Test2019.tf
@@ -156,14 +156,6 @@ module "test_2019_1_ec2" {
       volume_size           = "20"
       volume_type           = var.volume_type
       kms_key_id            = data.aws_kms_key.ebs.arn
-    },
-    {
-      delete_on_termination = var.delete_on_termination
-      device_name           = "/dev/xvdh"
-      encrypted             = var.ebs_encrypted
-      volume_size           = "11"
-      volume_type           = var.volume_type
-      kms_key_id            = data.aws_kms_key.ebs.arn
     }
   ]
 
