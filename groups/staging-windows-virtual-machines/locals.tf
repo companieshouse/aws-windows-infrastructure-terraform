f767c2f3629c61ab8814d25850b28e8c307c19b5
sijegbulem-ch <sijegbulem@companieshouse.gov.uk>
2026-03-18 14:02:40 +0000

    moved cidr to vault v1

--- groups/staging-windows-virtual-machines/locals.tf
+++ groups/staging-windows-virtual-machines/locals.tf
@@ -75,125 +75,84 @@ locals {
   # ABBYY Development Server Security Group Variables
   # ------------------------------------------------------------------------------
 
+  ingress_cidr_blocks_abby_dev = try(
+    jsondecode(data.vault_generic_secret.abbyy_dev_ec2_data.data["ingress-cidr-blocks"]),
+    data.vault_generic_secret.abbyy_dev_ec2_data.data["ingress-cidr-blocks"]
+  )
+
   # ABBYY Dev 135 port CIDR blocks
-  abbyy_dev_135_cidr_block = [
-    "172.16.202.91/32",
-    "172.16.101.82/32"
-  ]
+  abbyy_dev_135_cidr_block = local.ingress_cidr_blocks_abby_dev["abbyy_dev_135_cidr_block"]
 
   # ABBYY Dev 49155 port CIDR blocks
-  abbyy_dev_49155_cidr_block = [
-    "172.16.202.91/32",
-    "172.16.101.82/32"
-  ]
+  abbyy_dev_49155_cidr_block = local.ingress_cidr_blocks_abby_dev["abbyy_dev_49155_cidr_block"]
+
 
   # ------------------------------------------------------------------------------
   # Document Simulation Test Server Security Group Variables
   # ------------------------------------------------------------------------------
 
+  ingress_cidr_blocks_doc_sim = try(
+    jsondecode(data.vault_generic_secret.doc_sim_ec2_data.data["ingress-cidr-blocks"]),
+    data.vault_generic_secret.doc_sim_ec2_data.data["ingress-cidr-blocks"]
+  )
+
   # Document Simulation Test 135 port CIDR blocks
-  doc_sim_135_cidr_block = [
-    "172.16.202.91/32",
-    "172.16.101.82/32"
-  ]
+  doc_sim_135_cidr_block = local.ingress_cidr_blocks_doc_sim["doc_sim_135_cidr_block"]
 
   # Document Simulation Test winrm http port CIDR blocks
-  doc_sim_winrm_http_cidr_block = [
-    "172.16.200.200/32",
-    "172.16.155.9/32"
-  ]
+  doc_sim_winrm_http_cidr_block = local.ingress_cidr_blocks_doc_sim["doc_sim_winrm_http_cidr_block"]
 
   # Document Simulation Test 49155 port CIDR blocks
-  doc_sim_49155_cidr_block = [
-    "172.16.101.82/32",
-    "172.16.202.91/32"
-  ]
+  doc_sim_49155_cidr_block = local.ingress_cidr_blocks_doc_sim["doc_sim_49155_cidr_block"]
+
 
   # ------------------------------------------------------------------------------
   # Business Objects Server 2 Security Group Variables
   # ------------------------------------------------------------------------------
 
+  ingress_cidr_blocks_bus_obj = try(
+    jsondecode(data.vault_generic_secret.doc_sim_ec2_data.data["ingress-cidr-blocks"]),
+    data.vault_generic_secret.doc_sim_ec2_data.data["ingress-cidr-blocks"]
+  )
+
   # Business Objects server 2 135 port CIDR blocks
-  bus_obj_2_135_cidr_block = [
-    "172.16.202.91/32",
-    "172.16.101.82/32"
-  ]
+  bus_obj_2_135_cidr_block = local.ingress_cidr_blocks_bus_obj["bus_obj_2_135_cidr_block"]
 
   # Business Objects server 2 rdp port CIDR blocks
-  bus_obj_2_rdp_cidr_block = [
-    "172.16.101.82/32",
-    "10.172.120.168/29",
-    "10.172.24.168/29"
-  ]
+  bus_obj_2_rdp_cidr_block = local.ingress_cidr_blocks_bus_obj["bus_obj_2_rdp_cidr_block"]
 
   # Business Objects 49155 port CIDR blocks
-  bus_obj_2_49155_cidr_block = [
-    "172.16.101.82/32",
-    "172.16.202.91/32"
-  ]
+  bus_obj_2_49155_cidr_block = local.ingress_cidr_blocks_bus_obj["bus_obj_2_49155_cidr_block"]
 
   # Business Objects Server 2 8080 port CIDR blocks
-  bus_obj_2_8080_cidr_block = [
-    "10.172.0.0/17",
-    "172.16.0.0/16",
-    "172.17.0.0/16",
-    "172.18.0.0/16",
-    "172.19.0.0/16",
-    "172.23.0.0/16"
-  ]
+  bus_obj_2_8080_cidr_block = local.ingress_cidr_blocks_bus_obj["bus_obj_2_8080_cidr_block"]
 
   # Business Objects Server 2 6400 - 6414 port CIDR blocks
-  bus_obj_2_6400_6414_cidr_block = [
-    "10.172.0.0/17",
-    "172.16.0.0/16",
-    "172.17.0.0/16",
-    "172.18.0.0/16",
-    "172.19.0.0/16",
-    "172.23.0.0/16",
-    "10.80.10.0/24"
-  ]
+  bus_obj_2_6400_6414_cidr_block = local.ingress_cidr_blocks_bus_obj["bus_obj_2_6400_6414_cidr_block"]
+
 
   # ------------------------------------------------------------------------------
   # Test 2019 Server 1 Security Group Variables
   # ------------------------------------------------------------------------------
 
+  ingress_cidr_blocks_test_ec2 = try(
+    jsondecode(data.vault_generic_secret.test_2019_1_ec2_data.data["ingress-cidr-blocks"]),
+    data.vault_generic_secret.test_2019_1_ec2_data.data["ingress-cidr-blocks"]
+  )
+
   # Test 2019 server 1 135 port CIDR blocks
-  test_2019_1_135_cidr_block = [
-    "172.16.202.91/32",
-    "172.16.101.82/32"
-  ]
+  test_2019_1_135_cidr_block = local.ingress_cidr_blocks_test_ec2["test_2019_1_135_cidr_block"]
 
   # Test 2019 server 1 rdp port CIDR blocks
-  test_2019_1_rdp_cidr_block = [
-    "172.16.101.82/32",
-    "10.172.120.168/29",
-    "10.172.24.168/29"
-  ]
+  test_2019_1_rdp_cidr_block = local.ingress_cidr_blocks_test_ec2["test_2019_1_rdp_cidr_block"]
 
   # Test 2019 server 1 49155 port CIDR blocks
-  test_2019_1_49155_cidr_block = [
-    "172.16.101.82/32",
-    "172.16.202.91/32"
-  ]
+  test_2019_1_49155_cidr_block = local.ingress_cidr_blocks_test_ec2["test_2019_1_49155_cidr_block"]
 
   # Test 2019 server 1 8080 port CIDR blocks
-  test_2019_1_8080_cidr_block = [
-    "10.172.0.0/17",
-    "172.16.0.0/16",
-    "172.17.0.0/16",
-    "172.18.0.0/16",
-    "172.19.0.0/16",
-    "172.23.0.0/16"
-  ]
+  test_2019_1_8080_cidr_block = local.ingress_cidr_blocks_test_ec2["test_2019_1_8080_cidr_block"]
 
   # Test 2019 server 1 6400 - 6414 port CIDR blocks
-  test_2019_1_6400_6414_cidr_block = [
-    "10.172.0.0/17",
-    "172.16.0.0/16",
-    "172.17.0.0/16",
-    "172.18.0.0/16",
-    "172.19.0.0/16",
-    "172.23.0.0/16",
-    "10.80.10.0/24"
-  ]  
+  test_2019_1_6400_6414_cidr_block = local.ingress_cidr_blocks_test_ec2["test_2019_1_6400_6414_cidr_block"]
+
 }
