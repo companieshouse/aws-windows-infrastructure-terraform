0ca365b014b6d1086e21e1afe08d0f569840ac0d
Aidey200 <86227996+Aidey200@users.noreply.github.com>
2026-06-09 09:57:36 +0100

    test test2019v2 - deploy

--- groups/staging-windows-virtual-machines/Test2019v2.tf
+++ groups/staging-windows-virtual-machines/Test2019v2.tf
@@ -93,7 +93,6 @@ resource "aws_ebs_volume" "test_2019_2" {
     Backup         = "backup14"
     BackupApp      = var.application
     scheduled_stop = var.scheduled_stop
-    ServiceTeam    = "Windows"
     DevelopmentTeam =   "None" 
     Owner           =   "MSS"
   }
